69c76b857a81984e158b4322c66918702d556d8b
Adrian Kosmaczewski <akosma@gmail.com>
2009-11-04 11:27:10 +0100

    Added support to directly manipulate the Lorenz attractor with touch gestures

--- Classes/Views/EAGLView.h
+++ Classes/Views/EAGLView.h
@@ -11,15 +11,6 @@
 #import <OpenGLES/ES1/gl.h>
 #import <OpenGLES/ES1/glext.h>
 
-typedef enum __MOVMENT_TYPE 
-{
-    MTNone = 0,
-    MTWalkForward,
-    MTWAlkBackward,
-    MTTurnLeft,
-    MTTurnRight
-} MovementType;
-
 @interface EAGLView : UIView 
 {
 @private
@@ -35,15 +26,9 @@ typedef enum __MOVMENT_TYPE
     NSTimer *animationTimer;
     NSTimeInterval animationInterval;
 
-    // Where we are viewing from
-    GLfloat eye[3];
-    // Where we are looking towards
-    GLfloat center[3];
-
-    MovementType currentMovement;
+    GLfloat rotationAngles[3];
+    CGPoint startTouchPosition;
 }
-
-@property NSTimeInterval animationInterval;
 
 - (void)startAnimation;
 - (void)stopAnimation;
@@ -51,6 +36,5 @@ typedef enum __MOVMENT_TYPE
 
 - (void)setupView;
 - (void)checkGLError:(BOOL)visibleCheck;
-- (void)handleTouches;
 
 @end
--- Classes/Views/EAGLView.m
+++ Classes/Views/EAGLView.m
@@ -8,23 +8,13 @@
 
 #import <QuartzCore/QuartzCore.h>
 #import <OpenGLES/EAGLDrawable.h>
-
 #import "EAGLView.h"
 
-#define WALK_SPEED 0.1
-#define TURN_SPEED 0.1
-
+#define ROTATION_ANGLE_STEP 5.0
 #define USE_DEPTH_BUFFER 1
 #define DEGREES_TO_RADIANS(__ANGLE) ((__ANGLE) / 180.0 * M_PI)
 
-void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
-			   GLfloat centery, GLfloat centerz, GLfloat upx, GLfloat upy,
-			   GLfloat upz);
-
 @interface EAGLView ()
-
-@property (nonatomic, retain) EAGLContext *context;
-@property (nonatomic, assign) NSTimer *animationTimer;
 
 - (BOOL) createFramebuffer;
 - (void) destroyFramebuffer;
@@ -33,10 +23,6 @@ void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
 
 
 @implementation EAGLView
-
-@synthesize context;
-@synthesize animationTimer;
-@synthesize animationInterval;
 
 + (Class)layerClass 
 {
@@ -66,15 +52,10 @@ void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
         }
         
         animationInterval = 1.0 / 60.0;
-		currentMovement = MTNone;
-        
-        eye[0] = -35.0;
-        eye[1] = -20.5;
-        eye[2] = 80.0;
-        
-        center[0] = -5.0;
-        center[1] = 1.5;
-        center[2] = -10.0;
+
+        rotationAngles[0] = 0.0;
+        rotationAngles[1] = 0.0;
+        rotationAngles[2] = 0.0;
         
 		[self setupView];
     }
@@ -120,9 +101,9 @@ void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
     glDrawArrays(GL_POINTS, 0, points_count);
     
 	glLoadIdentity();
-    [self handleTouches];
-    gluLookAt(eye[0], eye[1], eye[2], center[0], center[1], center[2],
-              0.0, 1.0, 0.0);
+    glRotatef(rotationAngles[0], 1.0, 0.0, 0.0);
+    glRotatef(rotationAngles[1], 0.0, 1.0, 0.0);
+    glRotatef(rotationAngles[2], 0.0, 0.0, 1.0);
     
     glBindRenderbufferOES(GL_RENDERBUFFER_OES, viewRenderbuffer);
     [context presentRenderbuffer:GL_RENDERBUFFER_OES];
@@ -130,83 +111,36 @@ void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event 
 {
-    UITouch *t = [[touches allObjects] objectAtIndex:0];
-    CGPoint touchPos = [t locationInView:t.view];
-    
-    if (touchPos.y < 160) 
+    UITouch *t = [touches anyObject];
+    startTouchPosition = [t locationInView:t.view];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *t = [touches anyObject];
+    CGPoint endTouchPosition = [t locationInView:t.view];
+    if (startTouchPosition.x < endTouchPosition.x)
     {
-        // We are moving forward
-        currentMovement = MTWalkForward;
-        
-    } 
-    else if (touchPos.y > 320) 
-    {
-        // We are moving backward
-        currentMovement = MTWAlkBackward;
-        
-    } 
-    else if (touchPos.x < 160) 
-    {
-        // Turn left
-        currentMovement = MTTurnLeft;
-    } 
-    else 
-    {
-        // Turn Right
-        currentMovement = MTTurnRight;
+        rotationAngles[1] += ROTATION_ANGLE_STEP;
     }
+    else if (startTouchPosition.x > endTouchPosition.x)
+    {
+        rotationAngles[1] -= ROTATION_ANGLE_STEP;
+    }
+
+    if (startTouchPosition.y < endTouchPosition.y)
+    {
+        rotationAngles[0] += ROTATION_ANGLE_STEP;
+    }
+    else if (startTouchPosition.y > endTouchPosition.y)
+    {
+        rotationAngles[0] -= ROTATION_ANGLE_STEP;
+    }
+    startTouchPosition = endTouchPosition;
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event 
 {
-    currentMovement = MTNone;
-}
-
-- (void)handleTouches 
-{
-    if (currentMovement == MTNone) 
-    {
-        // We're going nowhere, nothing to do here
-        return;
-    }
-    
-    GLfloat vector[3];
-    
-    vector[0] = center[0] - eye[0];
-    vector[1] = center[1] - eye[1];
-    vector[2] = center[2] - eye[2];
-    
-    switch (currentMovement) 
-    {
-        case MTWalkForward:
-            eye[0] += vector[0] * WALK_SPEED;
-            eye[2] += vector[2] * WALK_SPEED;
-            center[0] += vector[0] * WALK_SPEED;
-            center[2] += vector[2] * WALK_SPEED;
-            break;
-            
-        case MTWAlkBackward:
-            eye[0] -= vector[0] * WALK_SPEED;
-            eye[2] -= vector[2] * WALK_SPEED;
-            center[0] -= vector[0] * WALK_SPEED;
-            center[2] -= vector[2] * WALK_SPEED;
-            break;
-            
-        case MTTurnLeft:
-            center[0] = eye[0] + cos(-TURN_SPEED)*vector[0] -
-            sin(-TURN_SPEED)*vector[2];
-            center[2] = eye[2] + sin(-TURN_SPEED)*vector[0] +
-            cos(-TURN_SPEED)*vector[2];
-            break;
-            
-        case MTTurnRight:
-            center[0] = eye[0] + cos(TURN_SPEED)*vector[0] - sin(TURN_SPEED)*vector[2];
-            center[2] = eye[2] + sin(TURN_SPEED)*vector[0] + cos(TURN_SPEED)*vector[2];
-            break;
-            
-        default:
-            break;
-    }
 }
 
 - (void)layoutSubviews 
@@ -225,23 +159,38 @@ void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
     
     glBindFramebufferOES(GL_FRAMEBUFFER_OES, viewFramebuffer);
     glBindRenderbufferOES(GL_RENDERBUFFER_OES, viewRenderbuffer);
-    [context renderbufferStorage:GL_RENDERBUFFER_OES fromDrawable:(CAEAGLLayer*)self.layer];
-    glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_COLOR_ATTACHMENT0_OES, GL_RENDERBUFFER_OES, viewRenderbuffer);
+    [context renderbufferStorage:GL_RENDERBUFFER_OES 
+                    fromDrawable:(CAEAGLLayer*)self.layer];
+    glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, 
+                                 GL_COLOR_ATTACHMENT0_OES, 
+                                 GL_RENDERBUFFER_OES, 
+                                 viewRenderbuffer);
     
-    glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_WIDTH_OES, &backingWidth);
-    glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_HEIGHT_OES, &backingHeight);
+    glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, 
+                                    GL_RENDERBUFFER_WIDTH_OES, 
+                                    &backingWidth);
+    glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, 
+                                    GL_RENDERBUFFER_HEIGHT_OES, 
+                                    &backingHeight);
     
     if (USE_DEPTH_BUFFER) 
     {
         glGenRenderbuffersOES(1, &depthRenderbuffer);
         glBindRenderbufferOES(GL_RENDERBUFFER_OES, depthRenderbuffer);
-        glRenderbufferStorageOES(GL_RENDERBUFFER_OES, GL_DEPTH_COMPONENT16_OES, backingWidth, backingHeight);
-        glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_DEPTH_ATTACHMENT_OES, GL_RENDERBUFFER_OES, depthRenderbuffer);
+        glRenderbufferStorageOES(GL_RENDERBUFFER_OES, 
+                                 GL_DEPTH_COMPONENT16_OES, 
+                                 backingWidth, 
+                                 backingHeight);
+        glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, 
+                                     GL_DEPTH_ATTACHMENT_OES, 
+                                     GL_RENDERBUFFER_OES, 
+                                     depthRenderbuffer);
     }
     
     if(glCheckFramebufferStatusOES(GL_FRAMEBUFFER_OES) != GL_FRAMEBUFFER_COMPLETE_OES) 
     {
-        NSLog(@"failed to make complete framebuffer object %x", glCheckFramebufferStatusOES(GL_FRAMEBUFFER_OES));
+        NSLog(@"failed to make complete framebuffer object %x", 
+              glCheckFramebufferStatusOES(GL_FRAMEBUFFER_OES));
         return NO;
     }
     
@@ -259,10 +208,16 @@ void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
 	
 	// This give us the size of the iPhone display
     CGRect rect = self.bounds;
-    glFrustumf(-size, size, -size / (rect.size.width / rect.size.height), size / (rect.size.width / rect.size.height), zNear, zFar);
+    glFrustumf(-size, 
+               size, 
+               -size / (rect.size.width / rect.size.height), 
+               size / (rect.size.width / rect.size.height), 
+               zNear, 
+               zFar);
     glViewport(0, 0, rect.size.width, rect.size.height);
 	
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);	
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glTranslatef(0.0, 0.0, -80.0);
 }
 
 - (void)destroyFramebuffer 
@@ -282,7 +237,7 @@ void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
 
 - (void)startAnimation 
 {
-    self.animationTimer = [NSTimer scheduledTimerWithTimeInterval:animationInterval 
+    animationTimer = [NSTimer scheduledTimerWithTimeInterval:animationInterval 
                                                            target:self 
                                                          selector:@selector(drawView) 
                                                          userInfo:nil 
@@ -291,23 +246,7 @@ void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
 
 - (void)stopAnimation 
 {
-    self.animationTimer = nil;
-}
-
-- (void)setAnimationTimer:(NSTimer *)newTimer 
-{
-    [animationTimer invalidate];
-    animationTimer = newTimer;
-}
-
-- (void)setAnimationInterval:(NSTimeInterval)interval 
-{
-    animationInterval = interval;
-    if (animationTimer) 
-    {
-        [self stopAnimation];
-        [self startAnimation];
-    }
+    animationTimer = nil;
 }
 
 - (void)checkGLError:(BOOL)visibleCheck 
@@ -354,75 +293,3 @@ void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
 }
 
 @end
-
-#pragma mark -
-#pragma mark SGI Copyright Functions
-
-static void normalize(float v[3])
-{
-    float r;
-	
-    r = sqrt( v[0]*v[0] + v[1]*v[1] + v[2]*v[2] );
-    if (r == 0.0) return;
-	
-    v[0] /= r;
-    v[1] /= r;
-    v[2] /= r;
-}
-
-static void __gluMakeIdentityf(GLfloat m[16])
-{
-    m[0+4*0] = 1; m[0+4*1] = 0; m[0+4*2] = 0; m[0+4*3] = 0;
-    m[1+4*0] = 0; m[1+4*1] = 1; m[1+4*2] = 0; m[1+4*3] = 0;
-    m[2+4*0] = 0; m[2+4*1] = 0; m[2+4*2] = 1; m[2+4*3] = 0;
-    m[3+4*0] = 0; m[3+4*1] = 0; m[3+4*2] = 0; m[3+4*3] = 1;
-}
-
-static void cross(float v1[3], float v2[3], float result[3])
-{
-    result[0] = v1[1]*v2[2] - v1[2]*v2[1];
-    result[1] = v1[2]*v2[0] - v1[0]*v2[2];
-    result[2] = v1[0]*v2[1] - v1[1]*v2[0];
-}
-
-void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
-			   GLfloat centery, GLfloat centerz, GLfloat upx, GLfloat upy,
-			   GLfloat upz)
-{
-    float forward[3], side[3], up[3];
-    GLfloat m[4][4];
-	
-    forward[0] = centerx - eyex;
-    forward[1] = centery - eyey;
-    forward[2] = centerz - eyez;
-	
-    up[0] = upx;
-    up[1] = upy;
-    up[2] = upz;
-	
-    normalize(forward);
-	
-    /* Side = forward x up */
-    cross(forward, up, side);
-    normalize(side);
-	
-    /* Recompute up as: up = side x forward */
-    cross(side, forward, up);
-	
-    __gluMakeIdentityf(&m[0][0]);
-    m[0][0] = side[0];
-    m[1][0] = side[1];
-    m[2][0] = side[2];
-	
-    m[0][1] = up[0];
-    m[1][1] = up[1];
-    m[2][1] = up[2];
-	
-    m[0][2] = -forward[0];
-    m[1][2] = -forward[1];
-    m[2][2] = -forward[2];
-	
-    glMultMatrixf(&m[0][0]);
-    glTranslatef(-eyex, -eyey, -eyez);
-}
-
